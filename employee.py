ed145b787091a30c836ebc90f0915ba9852ffb5a
Sai Anumala <154539727+sai-anumala@users.noreply.github.com>
2025-01-06 21:22:10 +0530

    employeeDB.py

---
+++ employee.py
@@ -0,0 +1,7 @@
+employees = {
+"E001": {"name": "Alice", "age": 30, "salary": 55000},
+"E002": {"name": "Bob", "age": 45, "salary": 75000},
+"E003": {"name": "Charlie", "age": 28, "salary": 48000},
+"E004": {"name": "David", "age": 35, "salary": 62000},
+"E005": {"name": "Eva", "age": 40, "salary": 69000}
+}
